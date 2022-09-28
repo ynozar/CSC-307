948bd8b1d5e60ee5b2793879f415193068ef66be
ynozar <ynozar@calpoly.edu>
2022-09-28 12:59:52 -0700

    post to backend

--- react-frontend/src/MyApp.js
+++ react-frontend/src/MyApp.js
@@ -23,15 +23,30 @@ function MyApp() {
               setCharacters(result);
          });
      }, [] );
+
+     async function makePostCall(person){
+        try {
+           const response = await axios.post('http://localhost:5000/users', person);
+           return response;
+        }
+        catch (error) {
+           console.log(error);
+           return false;
+        }
+     }
+     
       function removeOneCharacter (index) {
         const updated = characters.filter((character, i) => {
             return i !== index
           });
           setCharacters(updated);
         }
-        function updateList(person) {
-            setCharacters([...characters, person]);
-          }
+        function updateList(person) { 
+            makePostCall(person).then( result => {
+            if (result && result.status === 200)
+               setCharacters([...characters, person] );
+            });
+         }
             
         return (
             <div className="container">
