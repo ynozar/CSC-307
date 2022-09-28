4f140a99bf81decb4e570bbad2a7aa8a3bfe10a9
ynozar <ynozar@calpoly.edu>
2022-09-28 12:57:52 -0700

    import users from backend and CORS

--- expressjs-backend/backend.js
+++ expressjs-backend/backend.js
@@ -2,8 +2,10 @@ const e = require('express');
 const express = require('express');
 const app = express();
 const port = 5000;
-
+const cors = require('cors');
+app.use(cors());
 app.use(express.json());
+
 
 app.get('/', (req, res) => {
     res.send('Hello World!');
