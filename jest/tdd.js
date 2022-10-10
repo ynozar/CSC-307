3e4ee38ba326767d65635d6460a001589e7de066
ynozar <ynozar@calpoly.edu>
2022-10-09 20:50:20 -0700

    TDD Assignment

--- jest/tdd.js
+++ jest/tdd.js
@@ -1,11 +1,63 @@
 class Portfolio {
     constructor() {
-      this.stocks = new Map();
+      this.tickers = [];
+      this.numShares=[];
     }
+
+    isEmpty(){
+
+        return this.numShares.length==0? true :false ;
+    }
+    numUnique(){
+
+        return this.tickers.length
+    }
+
+    buy(ticker, numShares){
+        const inPort=this.tickers.indexOf(ticker)
+        if (inPort>-1){
+        this.numShares[inPort]+=numShares
+        }
+        else{
+        this.tickers.push(ticker)
+        this.numShares.push(numShares)}
+    }
+    sell(ticker, numShares){
+        const inPort=this.tickers.indexOf(ticker)
+        if (inPort>-1){
+        this.numShares[inPort]-=numShares
+        
+        if(this.numShares[inPort]<0){
+            throw 'ShareSaleException'
+        }
+        if(this.numShares[inPort]<1){
+            this.tickers.splice(inPort,1)
+            this.numShares.splice(inPort,1)
+        }
+
+
+        }
+        else{
+
+        }
+    }
+
+    getNumShares(ticker){
+        return this.numShares[this.tickers.indexOf(ticker)];
+    }
+    
+}
+
+
+
+  function sum(){ //DELETE
+
+    return new Portfolio();
+
   }
 
 
 
 exports.Portfolio = Portfolio;
 
-exports.Portfolio = Portfolio;
+exports.sum = sum;
--- jest/tdd.test.js
+++ jest/tdd.test.js
@@ -3,7 +3,85 @@ const myFunctions = require('./tdd.js');
 
 
 test('Testing sum -- success', () => {
-    const target = 30;
-    const result = myFunctions.sum(12, 18);
-    expect(target).toBe(result);
+    const target = new myFunctions.Portfolio();
+    const result = myFunctions.sum();
+    expect(target).toStrictEqual(result);
+  });
+
+test('Testing is empty -- success', () => {
+    const target = true;
+    const result = new myFunctions.Portfolio();
+    expect(target).toStrictEqual(result.isEmpty());
+  });
+
+
+test('Testing buy -- success', () => {
+
+    const target = {numShares: [5,10], tickers: ["GME","RBLX"],};
+    const result = new myFunctions.Portfolio();
+    result.buy("GME",5)
+    result.buy("RBLX",10)
+    expect(target).toEqual(expect.objectContaining(result));
+  });
+
+
+test('Testing unique -- success', () => {
+
+    const target = 2;
+    const result = new myFunctions.Portfolio();
+    result.buy("GME",5)
+    result.buy("RBLX",10)
+    expect(target).toBe(result.numUnique());
+  });
+
+
+test('Testing buy 2 -- success', () => {
+
+    const target = {numShares: [45,10], tickers: ["GME","RBLX"],};
+    const result = new myFunctions.Portfolio();
+    result.buy("GME",5)
+    result.buy("RBLX",10)
+    result.buy("GME",40)
+    expect(target).toEqual(expect.objectContaining(result));
+  });
+
+
+test('Testing sell  -- success', () => {
+
+    const target = {numShares: [2,10], tickers: ["GME","RBLX"],};
+    const result = new myFunctions.Portfolio();
+    result.buy("GME",5)
+    result.buy("RBLX",10)
+    result.sell("GME",3)
+    expect(target).toEqual(expect.objectContaining(result));
+  });
+
+
+test('Testing numShares  -- success', () => {
+
+    const target = 35;
+    const result = new myFunctions.Portfolio();
+    result.buy("GME",35)
+    result.buy("RBLX",10)
+    expect(target).toEqual((result.getNumShares("GME")));
+  });
+
+
+test('Testing remove ticker  -- success', () => {
+
+    const target = {numShares: [10], tickers: ["RBLX"],};
+    const result = new myFunctions.Portfolio();
+    result.buy("GME",5)
+    result.buy("RBLX",10)
+    result.sell("GME",5)
+    expect(target).toEqual(expect.objectContaining(result));
+  });
+
+
+test('Testing Sell Error  -- success', () => {
+    const result = new myFunctions.Portfolio();
+    result.buy("GME",35)
+    result.buy("RBLX",10)
+
+    expect(() => result.sell("RBLX",11)).toThrow(/ShareSaleException/);
   });
