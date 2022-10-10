b3b0aec5bd9ceca0c1680227abbdffaac621e56d
ynozar <ynozar@calpoly.edu>
2022-10-09 18:56:34 -0700

    start TDD

--- jest/tdd.test.js
+++ jest/tdd.test.js
@@ -1,2 +1,9 @@
 const { expect } = require('expect');
 const myFunctions = require('./tdd.js');
+
+
+test('Testing sum -- success', () => {
+    const target = 30;
+    const result = myFunctions.sum(12, 18);
+    expect(target).toBe(result);
+  });
